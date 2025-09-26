aaaa6c9fcccd0b1b9794c25466e71e0469a28838
Saachi <saachigupta04@gmail.com>
2025-09-25 18:15:20 -0600

    Resolved the conflict

--- app/src/main/java/com/example/pixels/Shape.java
+++ app/src/main/java/com/example/pixels/Shape.java
@@ -3,7 +3,7 @@ package com.example.pixels;
 public abstract class Shape {
     protected int x;
     protected int y;
-    String color = "phthalo green";
+    String color = "blue";
 
     public Shape(int x, int y) {
         this.x = x;
